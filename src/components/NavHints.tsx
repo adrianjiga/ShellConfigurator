8fa731d5cbf63cd41c56fee7e0f490dc620ab3c2
Adrian Jiga <jiga.ion.adrian@gmail.com>
2026-03-31 00:03:31 +0300

    Fix NavHints to use stable React key instead of array index
    Replace array index key with content-derived key from hint key and
    label to prevent React reconciliation issues on list reorder.

--- src/components/NavHints.tsx
+++ src/components/NavHints.tsx
@@ -13,8 +13,8 @@ interface NavHintsProps {
 export function NavHints({ hints }: NavHintsProps) {
   return (
     <Box marginTop={1}>
-      {hints.map((h, i) => (
-        <Box key={i} marginRight={2}>
+      {hints.map((h) => (
+        <Box key={h.key + h.label} marginRight={2}>
           <Text bold color="cyan">
             {h.key}
           </Text>
